b52200f8ae97d6dabf22e8cf048f0e76785c13b0
[Ollie Claridge] <[ollie.claridge@gmail.com]>
2023-05-15 10:49:31 +0100

    Header Layed Out

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './Header'
+
+function App() {
+
+  return(
+    <div className="header-container">
+        <Header />
+    </div>
+
+  )
+
+}
+
+export default App
---
+++ src/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Header() {
+  return (
+    
+        <>
+        <h1>Title</h1>
+        <div className='Nav'>
+        <p>Menu</p>
+        <p>Shopping Bag</p>
+        </div>
+         </>
+         
+      
+   
+  )
+}
+
+
+export default Header
